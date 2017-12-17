e74bfed24af5398fc35120baa1ce3a238c3589fd
Jakub Śliwa <jakubsliwa93@gmail.com>
2017-12-17 20:17:07 +0100

    Task 8 - 12 is done

---
+++ BasicExercises/Main11.java
@@ -0,0 +1,22 @@
+package BasicExercises;
+
+public class Main11 {
+
+	public static void main(String[] args) {
+		double radius = 7.5;
+		
+		double perimeter = 2 * Math.PI * radius;
+		double area = Math.PI * radius * radius;
+
+		System.out.println("Obwód to: " + perimeter);
+		System.out.println("Powierzchnia to: " + area);
+
+	}
+
+}
+
+/*
+ * Write a Java program to print the area and perimeter of a circle. Go to the
+ * editor Test Data: Radius = 7.5 Expected Output Perimeter is =
+ * 47.12388980384689 Area is = 176.71458676442586
+ */
---
+++ BasicExercises/Main12.java
@@ -0,0 +1,25 @@
+package BasicExercises;
+
+import java.util.Scanner;
+
+public class Main12 {
+
+	public static void main(String[] args) {
+
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Podaj pierwszą liczbę :");
+		int a = scan.nextInt();
+		System.out.println("Podaj drugą liczbę: ");
+		int b = scan.nextInt();
+		System.out.println("Podaj trzecią liczbę");
+		int c = scan.nextInt();
+		System.out.println("Wpisane liczby to: " + a + ", " + b + " " + c);
+		System.out.println("Obliczam średnią ...");
+		System.out.println("Średnia to: " + ((a + b + c) / 3));
+
+	}
+
+}
+
+// 12. Write a Java program that takes three numbers as input to calculate and
+// print the average of the numbers
