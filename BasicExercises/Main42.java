68507224012e226850bbac95f1179dc13c164274
Jakub Śliwa <jakubsliwa93@gmail.com>
2018-01-03 07:07:28 +0100

    Task 42 remains to done

--- BasicExercises/Main42.java
+++ BasicExercises/Main42.java
@@ -1,16 +1,27 @@
 package BasicExercises;
 
+/*import java.io.Console;*/
+
 public class Main42 {
 
 	public static void main(String[] args) {
-		int chr = 'A';
-		System.out.println("The ASCII value of Z is :" + chr);
+
+/*		Console cons = System.console();
+		if (cons == null) {
+			System.out.println("No console:");
+			System.exit(0);
+		}
+
+		String username = cons.readLine("Nazwa użytkownika: ");
+		System.out.println(username);
+		char[] passwd = cons.readPassword("Hasło: ");
+		System.out.println(passwd);*/
 	}
 }
 
 /*
- * 41. Write a Java program to print the ascii value of a given character. Go to
- * the editor Expected Output
+ * 42. Write a Java program to input and display your password. Go to the editor
+ * Expected Output
  * 
- * The ASCII value of Z is :90
+ * Input your Password: Your password was: abc@123
  */
