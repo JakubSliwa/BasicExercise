40733f5b8136d2a2bf0181ff068cd4e918918bc5
Jakub Śliwa <jakubsliwa93@gmail.com>
2018-01-03 19:57:40 +0100

    Task 51 is done

--- BasicExercises/Main51.java
+++ BasicExercises/Main51.java
@@ -1,8 +1,23 @@
 package BasicExercises;
 
+import java.util.Scanner;
+
 public class Main51 {
 
 	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		System.out.print("Input a number(string): ");
+		String string = scan.nextLine();
+		int result = Integer.parseInt(string);
+		System.out.printf("The integer value is: %d", result);
 
+		scan.close();
 	}
 }
+
+/*
+ * 51. Write a Java program to convert a string to an integer in Java. Go to the
+ * editor Sample Output:
+ * 
+ * Input a number(string): 25 The integer value is: 25
+ */
