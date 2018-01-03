928887134af3ba6c96d1be6a8ce74ca0b485a6d6
Jakub Śliwa <jakubsliwa93@gmail.com>
2018-01-03 19:26:11 +0100

    Task 48 is done

--- BasicExercises/Main48.java
+++ BasicExercises/Main48.java
@@ -1,13 +1,23 @@
 package BasicExercises;
 
-
-
 public class Main48 {
 
 	public static void main(String[] args) {
-
 		
 		
+		for(int i = 0; i <=100; i++) {
+			if(i % 2 == 1) {
+				System.out.println(i);
+			}
+		}
 	}
 }
 
+/*
+ * 48. Write a Java program to print the odd numbers from 1 to 99. Prints one
+ * number per line. Go to the editor Sample Output:
+ * 
+ * 1 3 5 7 9 11 ....
+ * 
+ * 91 93 95 97 99
+ */
