d84be7e118f695165fc4aca2db4d5efacce87cff
Jakub Śliwa <jakubsliwa93@gmail.com>
2018-01-04 07:08:39 +0100

    Task 56 is done

--- BasicExercises/Main55.java
+++ BasicExercises/Main55.java
@@ -1,8 +1,25 @@
 package BasicExercises;
 
+import java.util.Scanner;
+
 public class Main55 {
 
 	public static void main(String[] args) {
-
+		Scanner scan = new Scanner(System.in);
+		System.out.print("Podaj liczbę sekund: ");
+		int seconds = scan.nextInt();
+		int p1 = seconds % 60;
+		int p2 = seconds / 60;
+		int p3 = p2 % 60;
+		p2 = p2 / 60;
+		System.out.println(p2 + ":" + p3 + ":" + p1);
+		scan.close();
 	}
 }
+
+/*
+ * 55. Write a Java program to convert seconds to hour, minute and seconds. Go
+ * to the editor Sample Output:
+ * 
+ * Input seconds: 86399 23:59:59
+ */
