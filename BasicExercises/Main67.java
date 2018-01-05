0692587b2e96280914f77cec8709c1353686d217
Jakub Śliwa <jakubsliwa93@gmail.com>
2018-01-06 00:18:41 +0100

    Task 67 is done

--- BasicExercises/Main67.java
+++ BasicExercises/Main67.java
@@ -3,6 +3,16 @@ package BasicExercises;
 public class Main67 {
 
 	public static void main(String[] args) {
-
+		String main_string = "Python 3.0";
+		String word = "Tutorial";
+		System.out.println(main_string.substring(0, 7) + word + main_string.substring(6));
 	}
 }
+
+/*
+ * 67. Write a Java program to insert a word in the middle of the another
+ * string. Go to the editor Insert "Tutorial" in the middle of "Python 3.0", so
+ * result will be Python Tutorial 3.0 Sample Output:
+ * 
+ * Python Tutorial 3.0
+ */
