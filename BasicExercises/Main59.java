edd03f67f27020e7fabd00dad855f9c31ce85317
Jakub Śliwa <jakubsliwa93@gmail.com>
2018-01-05 22:45:16 +0100

    Task 59 is done

--- BasicExercises/Main59.java
+++ BasicExercises/Main59.java
@@ -1,8 +1,23 @@
 package BasicExercises;
 
+import java.util.Scanner;
+
 public class Main59 {
 
 	public static void main(String[] args) {
-
+		Scanner scan = new Scanner(System.in);
+		System.out.print("Input a Sentence: ");
+		String line = scan.nextLine();
+		String[] words = line.split("[ ]+");
+		System.out.println("Penultimate word: " + words[words.length - 2]);
+		scan.close();
 	}
 }
+
+/*
+ * 60. Write a Java program to find the penultimate (next to last) word of a
+ * sentence. Go to the editor Sample Output:
+ * 
+ * Input a String: The quick brown fox jumps over the lazy dog. Penultimate
+ * word: lazy
+ */
