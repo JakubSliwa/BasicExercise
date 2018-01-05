25ce974d06b0a6199d3077392d5321c2bf8a01d8
Jakub Śliwa <jakubsliwa93@gmail.com>
2018-01-05 23:15:24 +0100

    Task 62 is done

--- BasicExercises/Main62.java
+++ BasicExercises/Main62.java
@@ -1,8 +1,37 @@
 package BasicExercises;
 
+import java.util.Scanner;
+
 public class Main62 {
 
 	public static void main(String[] args) {
 
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Podaj pierwszą zmienną:");
+		int a = scan.nextInt();
+		System.out.println("Podaj drugą zmienną:");
+		int b = scan.nextInt();
+		System.out.println("Podaj trzecią zmienną:");
+		int c = scan.nextInt();
+
+		System.out.println(results(a, b, c));
+		scan.close();
+	}
+
+	public static boolean results(int a, int b, int c) {
+
+		if ((a - 20) > b || (a - 20) > c || (b - 20) > c) {
+			return true;
+		}
+		return false;
 	}
 }
+
+/*
+ * 62. Write a Java program that accepts three integer values and return true if
+ * one of them is 20 or more less than one of the others. Go to the editor
+ * Sample Output:
+ * 
+ * Input the first number : 15 Input the second number: 20 Input the third
+ * number : 25 false
+ */
