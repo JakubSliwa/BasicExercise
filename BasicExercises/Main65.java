2e520337bcfb3e7f72d56902b17713b592bbe7db
Jakub Śliwa <jakubsliwa93@gmail.com>
2018-01-06 00:11:30 +0100

    Task 66 is done

--- BasicExercises/Main65.java
+++ BasicExercises/Main65.java
@@ -1,8 +1,25 @@
 package BasicExercises;
 
+import java.util.Scanner;
+
 public class Main65 {
 
 	public static void main(String[] args) {
-
+		Scanner in = new Scanner(System.in);
+		System.out.print("Input the first number : ");
+		int a = in.nextInt();
+		System.out.print("Input the second number: ");
+		int b = in.nextInt();
+		int divided = a / b;
+		int result = a - (divided * b);
+		System.out.println(result);
+		in.close();
 	}
 }
+
+/*
+ * 65. Write a Java program to calculate the modules of two numbers without
+ * using any inbuilt modulus operator. Go to the editor Sample Output:
+ * 
+ * Input the first number : 19 Input the second number: 7 5
+ */
