aa2b7d66d6ed598f1dd0fff94d581d69910d6600
Jakub Śliwa <jakubsliwa93@gmail.com>
2018-01-05 23:03:40 +0100

    Task 61 is done

--- BasicExercises/Main61.java
+++ BasicExercises/Main61.java
@@ -4,5 +4,22 @@ public class Main61 {
 
 	public static void main(String[] args) {
 
+		String str = "qwertyuiop";
+		String reverse = reverseString(str);
+		System.out.println(reverse);
+	}
+
+	static String reverseString(String str) {
+		String reverse = "";
+		for (int i = str.length() - 1; i >= 0; i--) {
+			reverse += str.charAt(i);
+		}
+		return reverse;
 	}
 }
+
+/*
+ * 61. Write a Java program to reverse a word. Go to the editor Sample Output:
+ * 
+ * Input a word: dsaf Reverse word: fasd
+ */
