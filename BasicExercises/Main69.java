7b8b122fc749dfa57fe85c88cd657d48ffe9f56e
Jakub Śliwa <jakubsliwa93@gmail.com>
2018-01-06 10:48:45 +0100

    Task 69 is done

--- BasicExercises/Main69.java
+++ BasicExercises/Main69.java
@@ -4,5 +4,16 @@ public class Main69 {
 
 	public static void main(String[] args) {
 
+		String str = "JAVA .... ROCKS";
+		String copyStr = str.substring(0, str.length() / 2);
+
+		System.out.println(copyStr);
 	}
 }
+
+/*
+ * 69. Write a Java program to extract the first half of a string of even
+ * length. Go to the editor Test Data: Python Sample Output:
+ * 
+ * Pyt
+ */
