4a0eab651824561e3223378ef8ec6929510f5e11
Jakub Śliwa <jakubsliwa93@gmail.com>
2018-01-06 15:32:38 +0100

    Task 85 is done

--- BasicExercises/Main85.java
+++ BasicExercises/Main85.java
@@ -3,6 +3,22 @@ package BasicExercises;
 public class Main85 {
 
 	public static void main(String[] args) {
-
+		
+		String str1= "Hello how are you?";
+		System.out.println(str1.startsWith("Hello"));
+//		String str2= str1.substring(0,5);
+//		if(str2.equals("Hello")) {
+//			System.out.println(true);
+//		}else {
+//			System.out.println(false);
+//		}
 	}
 }
+
+
+/*85. Write a Java program to check if a string starts with a specified word. Go to the editor
+Sample Data: string1 = "Hello how are you?"
+Sample Output:
+
+true
+*/
