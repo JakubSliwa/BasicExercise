33f356643d5a7d109382e9c33ea2f5b974025bb0
Jakub Śliwa <jakubsliwa93@gmail.com>
2018-01-06 14:06:50 +0100

    Task 82 is done

--- BasicExercises/Main82.java
+++ BasicExercises/Main82.java
@@ -1,8 +1,33 @@
 package BasicExercises;
 
+import java.util.Arrays;
+
 public class Main82 {
 
 	public static void main(String[] args) {
 
+		int tab[] = { 20, 30, 80, 50, 67 };
+
+		System.out.println("Original Array: " + Arrays.toString(tab));
+		int max_val = tab[0];
+
+		if (tab[tab.length - 1] >= max_val) {
+			max_val = tab[tab.length - 1];
+		}
+		if (tab[tab.length / 2] >= max_val) {
+			max_val = tab[tab.length / 2];
+		}
+
+		System.out.println("Larger value between first and last element: " + max_val);
+
 	}
 }
+
+/*
+ * 82. Write a Java program to find the largest element between first, last, and
+ * middle values from an array of integers (even length). Go to the editor
+ * Sample Output:
+ * 
+ * Original Array: [20, 30, 40, 50, 67] Largest element between first, last, and
+ * middle values: 67
+ */
