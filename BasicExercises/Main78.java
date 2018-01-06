40bd90a1f04900c52de534a2e560057844cd925b
Jakub Śliwa <jakubsliwa93@gmail.com>
2018-01-06 13:38:51 +0100

    Task 78 is done

--- BasicExercises/Main78.java
+++ BasicExercises/Main78.java
@@ -4,5 +4,18 @@ public class Main78 {
 
 	public static void main(String[] args) {
 
+		int tab1[] = { 5, 3 };
+
+		if (tab1[0] == 4 || tab1[0] == 7)
+			System.out.println(true);
+		else
+			System.out.println(tab1[1] == 4 || tab1[1] == 7);
 	}
 }
+
+/*
+ * 78. Write a Java program to test that a given array of integers of length 2
+ * contains a 4 or a 7. Go to the editor Sample Output:
+ * 
+ * Original Array: [5, 7] true
+ */
