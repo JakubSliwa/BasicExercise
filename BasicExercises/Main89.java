677b86fef780d0614047ca3ee780b5130935679a
Jakub Śliwa <jakubsliwa93@gmail.com>
2018-01-06 18:38:43 +0100

    Task 89 is done

--- BasicExercises/Main89.java
+++ BasicExercises/Main89.java
@@ -3,6 +3,12 @@ package BasicExercises;
 public class Main89 {
 
 	public static void main(String[] args) {
-
+		System.out.println("System security interface:");
+		System.out.println(System.getSecurityManager());
 	}
 }
+
+/*
+ * 89. Write a Java program to check whether a security manager has already been
+ * established for the current application or not.
+ */
