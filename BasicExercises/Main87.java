de479dd9b2bbef18a527b98bf7dc9a5a9bb746bb
Jakub Śliwa <jakubsliwa93@gmail.com>
2018-01-06 18:35:30 +0100

    Task 87 is done

--- BasicExercises/Main87.java
+++ BasicExercises/Main87.java
@@ -3,6 +3,15 @@ package BasicExercises;
 public class Main87 {
 
 	public static void main(String[] args) {
-
+		int a = 50;
+		int b = 100;
+		System.out.println((a >= 1 && a <= 100) || (b >= 1 && b <= 100));
 	}
 }
+
+/*
+ * 87. Write a Java program to check if two numbers are in range 50..100
+ * inclusive. Go to the editor Sample Data: n1 = 50 n2 = 100 Sample Output:
+ * 
+ * false
+ */
