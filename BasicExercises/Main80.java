86a60ff681619a9ef4604aa6fc5267ce5e2ace1a
Jakub Śliwa <jakubsliwa93@gmail.com>
2018-01-06 13:53:34 +0100

    Task 80 is done

--- BasicExercises/Main80.java
+++ BasicExercises/Main80.java
@@ -1,8 +1,23 @@
 package BasicExercises;
 
+import java.util.Arrays;
+
 public class Main80 {
 
 	public static void main(String[] args) {
 
+		int tab[] = { 20, 30, 40 };
+		System.out.println("Original Array: " + Arrays.toString(tab));
+		int max_val = tab[0];
+		if (tab[2] >= max_val)
+			max_val = tab[2];
+		System.out.println("Larger value between first and last element: " + max_val);
 	}
 }
+
+/*
+ * 80. Write a Java program to get the larger value between first and last
+ * element of an array (length 3) of integers . Go to the editor Sample Output:
+ * 
+ * Orginal Array: [20, 30, 40] Larger value between first and last element: 40
+ */
