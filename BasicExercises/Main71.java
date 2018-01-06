829948097cc5ae668af2015dda270d36f52dba8a
Jakub Śliwa <jakubsliwa93@gmail.com>
2018-01-06 10:56:09 +0100

    Task 71 is done

--- BasicExercises/Main71.java
+++ BasicExercises/Main71.java
@@ -4,5 +4,22 @@ public class Main71 {
 
 	public static void main(String[] args) {
 
+		String str1 = "Java";
+		String str2 = "Turorial";
+
+		String str3 = str1.substring(1);
+		String str4 = str2.substring(1);
+
+		System.out.println(str3 + " " + str4);
+
 	}
 }
+
+/*
+ * 71. Write a Java program to create the concatenation of the two strings
+ * except removing the first character of each string. The length of the strings
+ * must be 1 and above. Go to the editor Test Data: Str1 = Python Str2 =
+ * Tutorial Sample Output:
+ * 
+ * ythonutorial
+ */
