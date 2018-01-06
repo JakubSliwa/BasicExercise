736fa7c3fdaac4f86b855d6551533ea37e327e05
Jakub Śliwa <jakubsliwa93@gmail.com>
2018-01-06 13:46:49 +0100

    Task 79 is done

--- BasicExercises/Main79.java
+++ BasicExercises/Main79.java
@@ -1,8 +1,23 @@
 package BasicExercises;
 
+import java.util.Arrays;
+
 public class Main79 {
 
 	public static void main(String[] args) {
 
+		int tab[] = { 20, 30, 40 };
+		System.out.println("Orginal tab: " + Arrays.toString(tab));
+
+		int tab2[] = { tab[1], tab[2], tab[0] };
+		System.out.println("Rotated tab: " + Arrays.toString(tab2));
+
 	}
 }
+
+/*
+ * 79. Write a Java program to rotate an array (length 3) of integers in left
+ * direction. Go to the editor Sample Output:
+ * 
+ * Original Array: [20, 30, 40] Rotated Array: [30, 40, 20]
+ */
