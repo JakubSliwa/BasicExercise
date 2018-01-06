2dffbd59b61ab28aed892bd0f3da8ea41fde78db
Jakub Śliwa <jakubsliwa93@gmail.com>
2018-01-06 11:03:33 +0100

    Task 72 is done

--- BasicExercises/Main72.java
+++ BasicExercises/Main72.java
@@ -4,5 +4,27 @@ public class Main72 {
 
 	public static void main(String[] args) {
 
+		String str1 = "";
+
+		int length = str1.length();
+		if (length >= 3) {
+			System.out.println(str1.substring(0, 3));
+		} else if (length == 1) {
+			System.out.println((str1.charAt(0) + "##"));
+		} else if (length == 2) {
+			System.out.println((str1.charAt(0) + "#"));
+		} else {
+			System.out.println("###");
+		}
+
 	}
 }
+
+/*
+ * 72. Write a Java program to create a new string taking first three characters
+ * from a given string. If the length of the given string is less than 3 use "#"
+ * as substitute characters. Go to the editor Test Data: Str1 = " " Sample
+ * Output:
+ * 
+ * ###
+ */
