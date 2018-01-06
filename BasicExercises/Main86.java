2bb4b6f60bc3984d8b9666e6d59c378c8c025f87
Jakub Śliwa <jakubsliwa93@gmail.com>
2018-01-06 15:39:44 +0100

    Task 86 is done

--- BasicExercises/Main86.java
+++ BasicExercises/Main86.java
@@ -4,5 +4,25 @@ public class Main86 {
 
 	public static void main(String[] args) {
 
+		int a, b;
+		a = 80;
+		b = 95;
+
+		System.out.println(check(a));
+		System.out.println(check(b));
+	}
+
+	public static boolean check(int x) {
+		if (x < 90 || x > 95) {
+			return true;
+		}
+		return false;
 	}
 }
+
+/*
+ * 86. Write a Java program to check if a number is less than 90 and greater
+ * than 95. Go to the editor Sample Data: n1 = 80 n2 = 95 Sample Output:
+ * 
+ * false
+ */
