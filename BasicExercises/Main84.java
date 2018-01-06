1bf6c85a5be6cca3ae56933d875ccdea5f239dc3
Jakub Śliwa <jakubsliwa93@gmail.com>
2018-01-06 15:01:47 +0100

    Task 84 is done

--- BasicExercises/Main84.java
+++ BasicExercises/Main84.java
@@ -4,5 +4,18 @@ public class Main84 {
 
 	public static void main(String[] args) {
 
+		String str = "Java is the best";
+		String str2 = str.substring(str.length() - 3);
+
+		System.out.println(str2 + str);
 	}
 }
+
+/*
+ * 84. Write a Java program to take the last three characters from a given
+ * string and add the three characters at both the front and back of the string.
+ * String length must be greater than three and more. Go to the editor Test
+ * data: "Python" will be "honPythonhon" Sample Output:
+ * 
+ * PyPythonPy
+ */
