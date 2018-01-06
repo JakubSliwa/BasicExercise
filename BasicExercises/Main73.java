3e480f4d1bc9a41d5b43524086f3c28c3fc82391
Jakub Śliwa <jakubsliwa93@gmail.com>
2018-01-06 12:24:15 +0100

    Task 73 is done

--- BasicExercises/Main73.java
+++ BasicExercises/Main73.java
@@ -4,5 +4,23 @@ public class Main73 {
 
 	public static void main(String[] args) {
 
+		String str1 = "Python";
+		String str2 = "";
+
+		int length2 = str2.length();
+		String result = "";
+		result += (str1.length() >= 1) ? str1.charAt(0) : '#';
+		result += (length2 >= 1) ? str2.charAt(length2 - 1) : '#';
+		System.out.println(result);
+
 	}
 }
+
+/*
+ * 73. Write a Java program to create a new string taking first and last
+ * characters from two given strings. If the length of either string is 0 use
+ * "#" for missing character. Go to the editor Test Data: str1 = "Python" str2 =
+ * " " Sample Output:
+ * 
+ * P#
+ */
