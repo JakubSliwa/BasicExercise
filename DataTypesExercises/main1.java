2e07de2324d60b2880a46379531b27ee5f97960c
Jakub Śliwa <jakubsliwa93@gmail.com>
2018-01-16 19:30:32 +0100

    Few changes

--- DataTypesExercises/main1.java
+++ DataTypesExercises/main1.java
@@ -8,3 +8,10 @@ public class main1 {
 	}
 
 }
+
+
+/*1. Write a Java program to convert temperature from Fahrenheit to Celsius degree. Go to the editor
+Test Data
+Input a degree in Fahrenheit: 212
+Expected Output:
+212.0 degree Fahrenheit is equal to 100.0 in Celsius*/
--- VariousTasks/Butelka.java
+++ VariousTasks/Butelka.java
@@ -1,18 +1,63 @@
 package VariousTasks;
 
-import java.util.Arrays;
-
 public class Butelka {
 
-	public static void main(String[] args) {
-		int ileButelek = 50;
-		Butelka[] butelka = new Butelka[ileButelek];
+	private double ileLitrów;
 
-		for (int i = 0; i <= ileButelek - 1; i++) {
-			butelka[i] = new Butelka();
-			System.out.println(butelka[i]);
-		}
+	Butelka(double ileLitrów) {
+		this.ileLitrów = ileLitrów;
+	}
+
+	double getIleLitrów() {
+		return ileLitrów;
+	}
+
+	void wlej(double ilość) {
+		this.ileLitrów += ilość;
 
 	}
 
+	boolean wylej(double ilość) {
+		if (ilość < ileLitrów) {
+			this.ileLitrów -= ilość;
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	void przelej(double ilość, Butelka gdziePrzelać) {
+		if (this.wylej(ilość)) {
+			gdziePrzelać.wlej(ilość);
+		} else {
+			System.out.println("Za mało");
+		}
+	}
+
+	public static void main(String[] args) {
+
+		Butelka[] butelka = new Butelka[3];
+
+		butelka[0] = new Butelka(3);
+		butelka[1] = new Butelka(2);
+		butelka[2] = new Butelka(1);
+
+		System.out.println(butelka[0].getIleLitrów());
+		System.out.println(butelka[1].getIleLitrów());
+		System.out.println(butelka[2].getIleLitrów());
+
+		butelka[0].wlej(4);
+		System.out.println(butelka[0].getIleLitrów());
+		butelka[0].przelej(12, butelka[2]);
+		System.out.println(butelka[0].getIleLitrów());
+		System.out.println(butelka[2].getIleLitrów());
+	}
+
 }
+
+/*
+ * int ileButelek = 50; Butelka[] butelka = new Butelka[ileButelek];
+ * 
+ * for (int i = 0; i <= ileButelek - 1; i++) { butelka[i] = new
+ * Butelka(ileLitrów); System.out.println(Arrays.deepToString(butelka));
+ */
