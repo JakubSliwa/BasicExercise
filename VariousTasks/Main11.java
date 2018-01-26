3bd8dc2655f3b9b22cbcd352754a100921704dc0
Jakub Śliwa <jakubsliwa93@gmail.com>
2018-01-26 11:37:34 +0100

    Task 11 is done

--- VariousTasks/Main11.java
+++ VariousTasks/Main11.java
@@ -3,8 +3,24 @@ package VariousTasks;
 public class Main11 {
 
 	public static void main(String[] args) {
-		
 
+		for (int i = 1; i <= 10; i++) {
+			System.out.println(i + " " + "Lubię Java");
+		}
+
+		int i = 10;
+		while (i > 0) {
+			System.out.println(i + " " + "Lubię Java");
+			i--;
+		}
 	}
 
 }
+
+/*
+ * Zadanie 3
+ * 
+ * W pliku Main3.java stwórz pętle, która 10 razy wypisze w konsoli tekst
+ * "Lubię Java". Napisz zarówno pętle for jak i pętlę while. W sumie napis
+ * będzie widoczny 20 razy.
+ */
