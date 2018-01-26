597b3b4c69e5b6dd27702b89af02444d382758bc
Jakub Śliwa <jakubsliwa93@gmail.com>
2018-01-26 12:40:22 +0100

    Task 16 is done

--- VariousTasks/Main16.java
+++ VariousTasks/Main16.java
@@ -4,6 +4,31 @@ public class Main16 {
 
 	public static void main(String[] args) {
 
+		for (int i = 1; i <= 5; i++) {
+			for (int j = 1; j <= 1; j++) {
+				System.out.println("* " + "* " + "* " + "* " + "* ");
+			}
+		}
 	}
 
 }
+
+/*
+ * Zadanie 8
+ * 
+ * W pliku Main8.java, jest napisany program, który wypisuje w konsoli schemat z
+ * n gwiazdek, taki jak poniżej. Przeanalizuj go bardzo dokładnie. Przykład dla
+ * n = 5:
+ * 
+ * * * * *
+ * 
+ * * * * *
+ * 
+ * * * * *
+ * 
+ * * * * *
+ * 
+ * * * * *
+ * 
+ * Używamy do tego zadania pętli zagnieżdżonych!
+ */
