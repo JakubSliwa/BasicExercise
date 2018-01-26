cbcdfb28a5747bcfdf31270c49fc2f449541a2dc
Jakub Śliwa <jakubsliwa93@gmail.com>
2018-01-26 11:51:01 +0100

    Task 13 is done

--- VariousTasks/Main13.java
+++ VariousTasks/Main13.java
@@ -4,6 +4,34 @@ public class Main13 {
 
 	public static void main(String[] args) {
 
+		int resultFor = 0;
+		int resultWhile = 0;
+
+		for (int i = 1; i <= 10; i++) {
+			int added = i;
+			resultFor += added;
+		}
+
+		int i = 1;
+		while (i <= 10) {
+			int added = i;
+			resultWhile += added;
+			i++;
+		}
+		System.out.println(resultFor);
+		System.out.println(resultWhile);
 	}
 
 }
+
+/*
+ * Zadanie 5
+ * 
+ * W pliku Main5.java stwórz zmienną resultFor oraz resultWhile i przypisz do
+ * nich liczbę 0. Następnie stwórz pętle, która doda do siebie liczby od 1 do 10
+ * i zapisze do zmiennej resultFor lub resultWhile. Pętla powinna co
+ * uruchomienie (zwaną także iteracją pętli) dodawać do zmiennej resultFor lub
+ * resultWhile kolejną liczbę z podanego zakresu. Napisz zarówno pętle for jak i
+ * pętlę while. Wypisz w konsoli w oddzielnych liniach zmienne resultFor oraz
+ * resultWhile.
+ */
