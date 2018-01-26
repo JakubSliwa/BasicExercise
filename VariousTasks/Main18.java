31ef7cb39bb9a6ad555605ac1fd807960a01bd41
Jakub Śliwa <jakubsliwa93@gmail.com>
2018-01-26 12:55:37 +0100

    Task 18 is done

--- VariousTasks/Main18.java
+++ VariousTasks/Main18.java
@@ -4,6 +4,37 @@ public class Main18 {
 
 	public static void main(String[] args) {
 
+		int n = 5;
+
+		for (int i = 0; i <= n; i++) {
+			String row = " ";
+			{
+				for (int j = 0; j <= n; j++) {
+					if (i >= j) {
+						row += "* ";
+					}
+				}
+			}
+			System.out.println(row);
+		}
 	}
 
 }
+
+/*
+ * Zadanie 10
+ * 
+ * W pliku Main10.java napisz program rysujący na podstawie wartości zmiennej n
+ * następujący schemat (tutaj dla n = 5).
+ *
+ * 
+ * * * * * * * * * * *
+ * 
+ * Możesz to zadanie rozwiązać na dwa sposoby:
+ * 
+ * używając pętli zależnych. używając pętli niezależnych i instrukcji warunkowej
+ * if.
+ * 
+ * Wzoruj się na poprzednich zadaniach. Trzeba tutaj trochę pomyśleć i rozumieć
+ * pętle zagnieżdżone.
+ */
