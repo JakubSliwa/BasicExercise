7a8f6e0b2aa6653b7bdcc7895f408c4ae304a34c
Jakub Śliwa <jakubsliwa93@gmail.com>
2018-01-26 12:50:48 +0100

    Task 16 and 17 is done

--- VariousTasks/Main17.java
+++ VariousTasks/Main17.java
@@ -4,6 +4,50 @@ public class Main17 {
 
 	public static void main(String[] args) {
 
-	}
+		int n = 10;
 
+		for (int i = 1; i <= n; i++) {
+			String row = "";
+			for (int j = 1; j <= n; j++) {
+				if (i >= j)
+					row += "* ";
+				else
+					row += j + " ";
+			}
+			System.out.print(row + " \n");
+		}
+
+		System.out.println();
+
+		n = 3;
+		for (int i = 1; i <= n; i++) {
+			String row = "";
+
+			for (int j = 1; j <= n; j++) {
+				if (i >= j)
+					row += "* ";
+				else
+					row += j + " ";
+			}
+			System.out.print(row + " \n");
+		}
+
+	}
 }
+
+/*
+ * W pliku Main9.java napisz program rysujący na podstawie wartości zmiennej n
+ * następujący schemat:
+ * 
+ * dla n = 5
+ * 
+ * 2 3 4 5 * 3 4 5 * * 4 5 * * * 5 * * * *
+ * 
+ * dla n = 3
+ * 
+ * 2 3 * 3 * *
+ * 
+ * Do stworzenia takiego zapisu w środku zagnieżdżonej pętli użyj wyrażenia
+ * warunkowego if dzięki któremu zdecydujesz czy wypisać gwiazdkę czy licznik z
+ * zagnieżdżonej pętli.
+ */
