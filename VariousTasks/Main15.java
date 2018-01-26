2c25f7b946e5f56a654dc8b16204561e476a0755
Jakub Śliwa <jakubsliwa93@gmail.com>
2018-01-26 12:36:38 +0100

    Task 15 is done

--- VariousTasks/Main15.java
+++ VariousTasks/Main15.java
@@ -4,6 +4,20 @@ public class Main15 {
 
 	public static void main(String[] args) {
 
+		for (int i = 0; i < 10; i++) {
+			for (int j = 0; j < 10; j++) {
+				System.out.println("i=" + i + " j=" + j);
+			}
+		}
+
 	}
 
 }
+
+/*
+ * Zadanie 7
+ * 
+ * W pliku Main7.java stwórz dwie pętle niezależne i wypisz wartości ich
+ * liczników w każdej iteracji. Wykorzystaj np.System.out.println("i= " +i +
+ * " j= "+j); Zadanie rozwiąż używając dwóch pętli for.
+ */
