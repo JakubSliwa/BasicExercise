08b0e258b217bb000b1abd3ab95bff27ee5c1574
Jakub Śliwa <jakubsliwa93@gmail.com>
2018-01-29 13:11:34 +0100

    Task 35 and 36 is done

--- VariousTasks/Main35.java
+++ VariousTasks/Main35.java
@@ -2,8 +2,21 @@ package VariousTasks;
 
 public class Main35 {
 
+	final static double USD = 3.34;
+
 	public static void main(String[] args) {
 
+		System.out.println(convertToUsd(1300));
 	}
 
+	private static double convertToUsd(double zlotys) {
+
+		return zlotys / USD;
+	}
 }
+
+/*
+ * W pliku Main3.java Napisz metodę convertToUsd, która przyjmuje parametr
+ * zlotys, czyli kwotę w złotówkach. Funkcja ma zwrócić podaną kwotę w dolarach
+ * amerykańskich. Jako przelicznik przyjmij wartość 4.04 PLN = 1 USD
+ */
--- VariousTasks/Main36.java
+++ VariousTasks/Main36.java
@@ -4,6 +4,20 @@ public class Main36 {
 
 	public static void main(String[] args) {
 
+		System.out.println(createName("Kuba", "Janusz", "Śliwa"));
 	}
 
+	private static String createName(String name, String surname, String nickname) {
+
+		return name + " \"" + nickname + "\" " + surname;
+	}
 }
+
+/*
+ * W pliku Main4.java Napisz metodę createName, która przyjmie następujące
+ * parametry:
+ * 
+ * name: imię, surname: nazwisko, nickname: pseudonim. Funkcja ma zwrócić
+ * łańcuch tekstowy z połączonymi parametrami w postaci Imię "Pseudonim"
+ * Nazwisko.
+ */
