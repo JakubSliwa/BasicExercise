d9db452f225fb7a6277de8f879369c06c559dcc6
Jakub Śliwa <jakubsliwa93@gmail.com>
2018-01-29 13:28:03 +0100

    Task 37 and 38 is done

--- VariousTasks/Main37.java
+++ VariousTasks/Main37.java
@@ -2,8 +2,22 @@ package VariousTasks;
 
 public class Main37 {
 
+	static final double VAT = 1.23;
+
 	public static void main(String[] args) {
 
+		System.out.println(calculateNet(500, VAT));
 	}
 
+	private static double calculateNet(double gross, double vat) {
+		return gross / vat;
+	}
 }
+
+/*
+ * W pliku Main5.java Napisz metodę calculateNet, która przyjmie argumenty:
+ * 
+ * gross, czyli kwotę brutto ceny zakupu, vat, czyli wartość podatku VAT. Możesz
+ * założyć, że VAT ma być liczbą zmiennoprzecinkową z zakresu 0 – 1. Funkcja ma
+ * zwrócić wartość netto ceny. Jakie obliczenia musisz wykonać?
+ */
--- VariousTasks/Main38.java
+++ VariousTasks/Main38.java
@@ -3,7 +3,25 @@ package VariousTasks;
 public class Main38 {
 
 	public static void main(String[] args) {
+		int myAge = 25;
+
+		System.out.println(checkMaturity(myAge));
 
 	}
 
+	private static boolean checkMaturity(int age) {
+		if (age >= 18) {
+			return true;
+		} else {
+			return false;
+		}
+	}
 }
+
+/*
+ * W pliku Main6.java Napisz metodę checkMaturity, która:
+ * 
+ * przyjmie parametr liczbowy age, który oznacza wiek użytkownika, sprawdzi czy
+ * użytkownik jest pełnoletni, jeśli jest – zwróci wartość true, jeśli nie –
+ * zwróci wartość false.
+ */
