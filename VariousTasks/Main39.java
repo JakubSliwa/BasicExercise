63db1b2fa94687a80ef939f6b3611bf0cfc49625
Jakub Śliwa <jakubsliwa93@gmail.com>
2018-01-29 14:10:36 +0100

    Task 39 and 40 is done

--- VariousTasks/Main39.java
+++ VariousTasks/Main39.java
@@ -4,6 +4,22 @@ public class Main39 {
 
 	public static void main(String[] args) {
 
+		System.out.println(checkEven(1111));
 	}
 
+	private static String checkEven(int number) {
+		if (number % 2 == 0) {
+			return "Parzysta";
+		} else {
+			return "Nieparzysta";
+		}
+	}
 }
+
+/*
+ * W pliku Main7.java Napisz metodę checkEven, która:
+ * 
+ * przyjmie parametr liczbowy number, będzie zwracać wartość typu String, jeśli
+ * liczba jest parzysta, zwróci wynik "even", jeśli liczba jest nieparzysta,
+ * zwróci wynik "odd".
+ */
--- VariousTasks/Main40.java
+++ VariousTasks/Main40.java
@@ -4,6 +4,24 @@ public class Main40 {
 
 	public static void main(String[] args) {
 
+		System.out.println(maxOfThree(-11, -5, -3));
 	}
 
+	private static int maxOfThree(int a, int b, int c) {
+		int max = a;
+
+		if (b > max) {
+			max = b;
+		}
+
+		if (c > max) {
+			max = c;
+		}
+		return max;
+	}
 }
+
+/*
+ * W pliku Main8.java Napisz metodę maxOfThree, która przyjmie trzy parametry
+ * liczbowe. Funkcja ma zwrócić największą liczbę.
+ */
