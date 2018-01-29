f30899fbc1beca96b1906c2c482ec29f2a7c7154
Jakub Śliwa <jakubsliwa93@gmail.com>
2018-01-29 11:18:11 +0100

    Task 25 and 26 is done

--- VariousTasks/Main25.java
+++ VariousTasks/Main25.java
@@ -3,8 +3,19 @@ package VariousTasks;
 public class Main25 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		int[] onlyThree = { 1, 2, 3 };
+
+		System.out.println(onlyThree[0] + ", " + onlyThree[1] + ", " + onlyThree[2]);
 
 	}
 
 }
+
+/*
+ * W pliku Main1.java
+ * 
+ * Stwórz tablicę z trzema elementami np. z liczbami, zmienną z tablicą nazwij
+ * onlyThree. Następnie wypisz na konsoli po kolei elementy tej tablicy
+ * rozdzielając je spacją. Nie korzystaj w tym zadaniu z pętli.
+ */
--- VariousTasks/Main26.java
+++ VariousTasks/Main26.java
@@ -3,8 +3,28 @@ package VariousTasks;
 public class Main26 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		String[] fruits = { "truskawka", "pomarańcza", "banan", "malina" };
+
+		System.out.println(fruits[0]);
+		System.out.println(fruits[fruits.length - 1]);
+
+		for (int i = 0; i <= fruits.length - 1; i++) {
+			System.out.println("--------");
+			System.out.println(fruits[i]);
+		}
 	}
 
 }
+
+/*
+ * W pliku Main2.java stwórz tablicę z listą swoich ulubionych owoców, zmienną z
+ * tablicą nazwij fruits.
+ * 
+ * Następnie:
+ * 
+ * Wypisz pierwszy owoc na stronie wg. wzoru: Pierwszy owoc to: truskawka.
+ * Wypisz ostatni owoc na stronie wg. wzoru: Ostatni owoc to: malina (skorzystaj
+ * z length). W pętli wypisz wszystkie owoce każdy w nowej linii (skorzystaj z
+ * length).
+ */
