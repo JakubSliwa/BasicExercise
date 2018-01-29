e8df087255d31133a55fdf607be84baa944874df
Jakub Śliwa <jakubsliwa93@gmail.com>
2018-01-29 11:08:02 +0100

    Task 24 is done

--- VariousTasks/Main24.java
+++ VariousTasks/Main24.java
@@ -1,10 +1,55 @@
 package VariousTasks;
 
+import java.util.Scanner;
+
 public class Main24 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner scan = new Scanner(System.in);
 
+		int a;
+		int b;
+
+		System.out.println("Podaj pierwszą liczbę");
+		a = getNumbers(scan);
+
+		System.out.println("Podaj drugą liczbę");
+		b = getNumbers(scan);
+
+		System.out.println("Podałeś: " + a + " i " + b);
+
+		System.out.println("Największy wspólny dzielnik tych liczby to : " + getGCD(a, b));
+
+		scan.close();
+	}
+
+	private static int getNumbers(Scanner scan) {
+		while (!scan.hasNextInt()) {
+			scan.next();
+			System.out.println("Podaj liczbę całkowitą");
+		}
+		return scan.nextInt();
+	}
+
+	private static int getGCD(int a, int b) {
+		int temporary;
+		while (b != 0) {
+
+			temporary = a;
+			a = b;
+			b = temporary % b;
+		}
+		return a;
 	}
 
 }
+
+/*
+ * W pliku Main16.java
+ * 
+ * Dodaj 2 zmiennej x i y będące dodatnimi liczbami całkowitymi. Napisz program
+ * wyliczający największy wspólny dzielnik (greatest common divisor - GDC) dwóch
+ * liczb. Skorzystaj z opisu na tej stronie. Wynik wyświetl na stronie wraz z
+ * liczbami, dla których został wyliczony wg. wzoru Dla liczb 420 i 168 GDC to:
+ * 84
+ */
