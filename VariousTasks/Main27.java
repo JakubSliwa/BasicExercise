2e0ea55f79dcc7ae679d1799a708d77753db9167
Jakub Śliwa <jakubsliwa93@gmail.com>
2018-01-29 11:29:49 +0100

    Task 27 and 28 is done

--- VariousTasks/Main27.java
+++ VariousTasks/Main27.java
@@ -3,8 +3,24 @@ package VariousTasks;
 public class Main27 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 10, 123 };
+
+		int sum = 0;
+
+		for (int i = 0; i < numbers.length; i++) {
+			sum += numbers[i];
+		}
+		System.out.println("Suma to: " + sum);
 	}
 
 }
+
+/*
+ * W pliku Main3.java
+ * 
+ * Stwórz tablicę z 10 dowolnymi liczbami, zmienną z tablicą nazwij numbers.
+ * Stwórz zmienną sum i przypisz do niej liczbę 0. Za pomocą pętli for oblicz
+ * sumę liczb z tablicy. Wypisz sumę na stronie wg. wzoru: Suma elementów
+ * tablicy to: 381.
+ */
--- VariousTasks/Main28.java
+++ VariousTasks/Main28.java
@@ -3,8 +3,36 @@ package VariousTasks;
 public class Main28 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 10, 123 };
+		int sumOdd = 0;
+
+		for (int i = 0; i < numbers.length; i++) {
+			if (numbers[i] % 2 == 0) {
+				System.out.println(numbers[i] + " jest liczbą parzystą");
+			} else {
+				System.out.println(numbers[i] + " jest liczbą nieparzystą");
+			}
+		}
+		for (int i = 0; i < numbers.length; i++) {
+			if (numbers[i] % 2 == 1) {
+				sumOdd += numbers[i];
+			}
+
+		}
+		System.out.println("Suma liczb nieparzystych to: " + sumOdd);
 	}
 
 }
+
+/*
+ * Zadanie 4
+ * 
+ * W pliku Main4.java
+ * 
+ * Stwórz tablicę z 10 dowolnymi liczbami, zmienną z tablicą nazwij numbers. W
+ * pętli for sprawdź, które są parzyste i wypisz je na stronie jedna pod drugą.
+ * Dodaj dodatkową zmienną sumOdd o wartości 0. Zmodyfikuj pętlę z pkt.1 w ten
+ * sposób aby dodatkowo sumowała liczby nieparzyste. Wyświetl dodatkowo na
+ * stronie komunikat wg. wzoru: Suma liczb nieparzystych to: 87.
+ */
