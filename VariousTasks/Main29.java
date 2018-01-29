94c794e6f5372662cea37608e94b32015ed041cb
Jakub Śliwa <jakubsliwa93@gmail.com>
2018-01-29 11:45:02 +0100

    Task 29 and 30 is done

--- VariousTasks/Main29.java
+++ VariousTasks/Main29.java
@@ -4,6 +4,32 @@ public class Main29 {
 
 	public static void main(String[] args) {
 
+		int[] numbers = { -132, -32, -999, -41, -8934, -434, -9, -3, -3322, -324 };
+
+		int max = numbers[0];
+
+		for (int i = 0; i < numbers.length; i++) {
+
+			if (numbers[i] > max) {
+				max = numbers[i];
+			}
+		}
+		System.out.println(max);
 	}
 
 }
+
+/*
+ * adanie 5
+ * 
+ * W pliku Main5.java
+ * 
+ * Stwórz tablicę z 10 dowolnymi liczbami, zmienną z tablicą nazwij numbers.
+ * Stwórz zmienną max i przypisz do niej 0. W pętli for znajdź liczbę, która
+ * jest największa, przypisz ją do zmiennej max i wypisz na stronie. Aby to
+ * zrobić przeiteruj tablicę i sprawdź czy aktualny element jest większy o
+ * aktualnej wartości max i jeśli tak, zaktualizuj zmienną max. Sprawdź czy Twój
+ * kod będzie działał poprawnie jeśli w tablicy znajdą się tylko liczby ujemne.
+ * Spróbuj zmodyfikować kod aby działał on uniwersalnie (podpowiedź: wystarczy
+ * zmodyfikować 1 linię kodu).
+ */
--- VariousTasks/Main30.java
+++ VariousTasks/Main30.java
@@ -3,7 +3,19 @@ package VariousTasks;
 public class Main30 {
 
 	public static void main(String[] args) {
+		int[] numbers = { -132, -32, -999, -41, -8934, -434, -9, -3, -3322, -324 };
 
+		for (int i = numbers.length - 1; i >= 0; i--) {
+
+			System.out.println(numbers[i]);
+		}
 	}
 
 }
+
+/*
+ * W pliku Main6.java
+ * 
+ * Stwórz tablicę z 10 dowolnymi liczbami, zmienną z tablicą nazwij numbers.
+ * Wypisz ją od tyłu używając pętli for, każdy element w nowej linii.
+ */
