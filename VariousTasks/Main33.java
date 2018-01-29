986e0904e07f48a0f2e34e5b5072538948fcfb36
Jakub Śliwa <jakubsliwa93@gmail.com>
2018-01-29 12:44:04 +0100

    Task 33 and 34 is done

--- VariousTasks/Main33.java
+++ VariousTasks/Main33.java
@@ -3,7 +3,21 @@ package VariousTasks;
 public class Main33 {
 
 	public static void main(String[] args) {
-
+		System.out.println(square(2));
 	}
 
+	private static int square(int num) {
+
+		return num * num;
+
+	}
 }
+
+/*
+ * #### Zadanie 1 - rozwiązywane z wykładowcą.
+ * 
+ * W pliku `Main1.java`
+ * 
+ * 1. Napisz metodę `static int square(int num)`, która zwróci wartość `num`
+ * podniesioną do kwadratu.
+ */
--- VariousTasks/Main34.java
+++ VariousTasks/Main34.java
@@ -3,7 +3,18 @@ package VariousTasks;
 public class Main34 {
 
 	public static void main(String[] args) {
-
+		System.out.println(multiply(2, 10));
 	}
 
+	private static int multiply(int multipler, int index) {
+
+		return multipler * index;
+	}
 }
+
+/*
+ * W pliku Main2.java
+ * 
+ * Napisz metodę static int multiply(int multipler, int index), która zwróci
+ * wartość zmiennej multipler pomnożoną przez wartość argumentu index.
+ */
