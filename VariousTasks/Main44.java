4e76b075f6b07e7028a95f4b2cbe51206cebd8f4
Jakub Śliwa <jakubsliwa93@gmail.com>
2018-01-30 19:19:42 +0100

    Task 44 is done

--- VariousTasks/Main44.java
+++ VariousTasks/Main44.java
@@ -1,9 +1,34 @@
 package VariousTasks;
 
+import java.util.Arrays;
+
 public class Main44 {
 
 	public static void main(String[] args) {
 
+		int[] tab = new int[10];
+		int[] tab2 = new int[10];
+
+		int i = 0;
+		while (i < 10) {
+			tab[i] = 2;
+			i++;
+		}
+
+		Arrays.fill(tab2, 2);
+		System.out.println(Arrays.toString(tab2));
+
+		System.out.println(Arrays.toString(tab));
 	}
 
 }
+
+/*
+ * #### Zadanie 3
+ * 
+ * W pliku `Main3.java`
+ * 
+ * 1. Stwórz 10 elementową tablicę wypełnioną pojedynczą wartością 2. Tablica
+ * powinna wyglądać następująco: ```` [2. 2. 2. 2. 2. 2 .2 . 2. 2. 2] ```` 2.
+ * Zadanie wykonaj bez użycia pętli for.
+ */
