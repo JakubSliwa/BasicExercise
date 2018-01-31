49df69b195c43f5ae6f7a0892eb147aa130c0b4b
Jakub Śliwa <jakubsliwa93@gmail.com>
2018-01-31 20:32:04 +0100

    Task 57 is done

--- VariousTasks/Main57.java
+++ VariousTasks/Main57.java
@@ -4,6 +4,31 @@ public class Main57 {
 
 	public static void main(String[] args) {
 
+		String str = "Coderslab";
+
+		String halfStr = firstHalf(str);
+
+		System.out.println(halfStr);
+
 	}
 
+	private static String firstHalf(String str) {
+
+		int strLen = (str.length()) / 2;
+		String firstHalfStr = str.substring(0, strLen);
+
+		return firstHalfStr;
+
+	}
 }
+
+/*
+ * #### Zadanie 6
+ * 
+ * W pliku `Main6.java` została umieszczona metoda o sygnaturze `static String
+ * firstHalf(String str)`.
+ * 
+ * 1. Uzupełnij ciało metody tak aby zwracała połowę napisu. 2. Uwzględniamy
+ * tylko część całkowitą podziału. Przykład: dla napisu `CodersLab` zwróci
+ * `Code`.
+ */
