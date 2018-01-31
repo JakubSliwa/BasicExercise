aa85aa8ee4cce6adab424dab6f0e10d3ea8a7e76
Jakub Śliwa <jakubsliwa93@gmail.com>
2018-01-31 20:24:14 +0100

    Task 56 is done

--- VariousTasks/Main56.java
+++ VariousTasks/Main56.java
@@ -4,6 +4,24 @@ public class Main56 {
 
 	public static void main(String[] args) {
 
+		String str = "Main Java";
+		char c = 'z';
+
+		System.out.println(charPos(str, c));
 	}
 
+	private static int charPos(String str, char c) {
+
+		return str.indexOf(c) + 1;
+	}
 }
+
+/*
+ * #### Zadanie 5
+ * 
+ * W pliku `Main5.java` została umieszczona metoda o sygnaturze `static int
+ * charPos(String str, char c) `.
+ * 
+ * 1. Uzupełnij ciało metody tak aby zwracała pozycje na której w ciągu `str`
+ * znajduje się znak `c`.
+ */
