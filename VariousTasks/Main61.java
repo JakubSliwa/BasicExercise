66ed047debe1749c5e2d3bfd1bbfe5a030e1ca0b
Jakub Śliwa <jakubsliwa93@gmail.com>
2018-01-31 21:54:04 +0100

    Task 61 is done

--- VariousTasks/Main61.java
+++ VariousTasks/Main61.java
@@ -3,7 +3,23 @@ package VariousTasks;
 public class Main61 {
 
 	public static void main(String[] args) {
+		String str = "Naucz się programować od podstaw. Rozwiń wymarzoną karierę w branży IT. ";
+
+		String[] tab = str.split("\\.");
+		for (String elem : tab) {
+			System.out.println(elem);
+		}
 
 	}
 
 }
+
+/*
+ * #### Zadanie 2
+ * 
+ * W pliku `Main2.java` napisz program, który:
+ * 
+ * 1. Dla znajdującej się w pliku zmiennej `str` podzieli napis na zdania.
+ * Znakiem podziału jest kropka `(.)` . 2. Wyświetli na konsoli każde zdanie w
+ * oddzielnej lini.
+ */
