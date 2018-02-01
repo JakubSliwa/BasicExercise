815785388a67e4a0a189f7e67de4a76a4f3ce5b5
Jakub Śliwa <jakubsliwa93@gmail.com>
2018-02-01 19:37:46 +0100

    Task 67 is done

--- VariousTasks/Main67.java
+++ VariousTasks/Main67.java
@@ -3,7 +3,28 @@ package VariousTasks;
 public class Main67 {
 
 	public static void main(String[] args) {
+		showLength("Coders lab");
+		showLength(null);
+		showLength("lab");
+	}
 
+	static void showLength(String s) {
+		try {
+			System.out.println(s.length());
+		} catch (NullPointerException e) {
+			System.out.println("Wartość nierozpoznawalna przez string");
+			e.printStackTrace();
+
+		}
+		System.out.println(s);
 	}
 
 }
+
+/*
+ * #### Zadanie 3
+ * 
+ * W pliku `Main3.java` znajduje się program, który zwraca wyjątek.
+ * 
+ * 1. Popraw kod dodając obsługę zwracanego wyjątku.
+ */
