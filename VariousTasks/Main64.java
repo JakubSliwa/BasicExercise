cb0e0a641b4888ff04114c9d2c3bc6133f330fca
Jakub Śliwa <jakubsliwa93@gmail.com>
2018-02-01 19:08:21 +0100

    Task 64 & 65 is done

--- VariousTasks/Main64.java
+++ VariousTasks/Main64.java
@@ -1,9 +1,25 @@
 package VariousTasks;
 
+import java.util.Date;
+
 public class Main64 {
 
 	public static void main(String[] args) {
+		Date data = new Date();
 
+		System.out.println(data);
 	}
 
 }
+
+/*
+ * 1. Importuje klasę `Date` z pakietu java.util a następnie wyświetla na
+ * konsoli: `System.out.println(new Date());`
+ */
+
+// #### Zadanie 2
+//
+// W pliku `Main2.java` napisz program który:
+//
+// 1. Wyświetli na konsoli nowo utworzony obiekt klasy Date z pakietu java.util.
+// 2. Istniejące w klasie instrukcje `import` mają pozostać
--- VariousTasks/Main65.java
+++ VariousTasks/Main65.java
@@ -1,9 +1,47 @@
 package VariousTasks;
 
+import java.util.Arrays;
+import java.util.Random;
+
 public class Main65 {
 
 	public static void main(String[] args) {
 
+		int[] newTab = createTab(5);
+		System.out.println(Arrays.toString(newTab));
+
+		try {
+			newTab[10] = 10;
+		} catch (ArrayIndexOutOfBoundsException e) {
+			System.err.println("Przekraczasz wielkość tabeli!");
+			e.printStackTrace();
+		} catch (Exception e) {
+
+			e.printStackTrace();
+		}
+
+	}
+
+	private static int[] createTab(int n) {
+		Random rand = new Random();
+
+		int[] tab = new int[n];
+
+		for (int i = 0; i < tab.length; i++) {
+			tab[i] = rand.nextInt(5);
+		}
+		return tab;
 	}
 
 }
+
+/*
+ * #### Zadanie 1 - rozwiązywane z wykładowcą.
+ * 
+ * W pliku `Main1.java` napisz program który:
+ * 
+ * 1. Do kodu znajdującego się w pliku dodaj obsługę wyjątku
+ * ArrayIndexOutOfBoundsException.
+ * 
+ * -----------------------------------------------------------------------------
+ */
