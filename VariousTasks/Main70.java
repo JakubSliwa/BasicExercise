1e506d6381b9ffe0d8b40cbb3ba18b9375a0ef96
Jakub Śliwa <jakubsliwa93@gmail.com>
2018-02-01 20:06:50 +0100

    Task 70 is done

--- VariousTasks/Main70.java
+++ VariousTasks/Main70.java
@@ -3,7 +3,23 @@ package VariousTasks;
 public class Main70 {
 
 	public static void main(String[] args) {
+		String str = null;
 
+		try {
+			System.out.println(str.length());
+		} catch (NullPointerException e) {
+			System.out.println("Wartość null");
+			e.printStackTrace();
+			throw e;
+		}
 	}
 
 }
+
+/*
+ * #### Zadanie 6
+ * 
+ * W pliku `Main6.java` znajduje się program, który zwraca wyjątek.
+ * 
+ * 1. Popraw kod dodając obsługę zwracanego wyjątku.
+ */
