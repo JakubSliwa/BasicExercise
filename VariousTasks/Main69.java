1c4c7b3b0b7789149fe68acb1326e30b9d582f0c
Jakub Śliwa <jakubsliwa93@gmail.com>
2018-02-01 20:03:21 +0100

    Task 69 is done

--- VariousTasks/Main69.java
+++ VariousTasks/Main69.java
@@ -1,9 +1,27 @@
 package VariousTasks;
 
+import static java.lang.Integer.parseInt;
+
 public class Main69 {
 
 	public static void main(String[] args) {
+		int num;
+		try {
+			num = parseInt(null);
+		} catch (NumberFormatException e) {
+			System.out.println("Nie da się strig przetworzyć na int");
 
+			throw e;
+		}
+		System.out.println(num);
 	}
 
 }
+
+/*
+ * #### Zadanie 5
+ * 
+ * W pliku `Main5.java` znajduje się program, który zwraca wyjątek.
+ * 
+ * 1. Popraw kod dodając obsługę zwracanego wyjątku.
+ */
